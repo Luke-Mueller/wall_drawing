dc8c66636cadca8e59715241decd60fac29f6bd3
Luke Mueller <lukemueller8@gmail.com>
2021-11-05 12:31:37 -0500

    removes dist dir

--- WallDrawing/dist/index.js
+++
@@ -1,7 +0,0 @@
-import WallDrawing from "./WallDrawing.js";
-import { selector } from './constants.js';
-let container;
-window.onload = () => {
-    container = document.querySelector(selector);
-    new WallDrawing(container);
-};
