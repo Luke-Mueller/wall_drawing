a324a81290e548a12304c1945a9ed6637542fcf5
Luke Mueller <lukemueller8@gmail.com>
2021-11-05 17:56:48 -0500

    hardcodes constants

--- WallDrawing/src/constants.ts
+++ WallDrawing/src/constants.ts
@@ -1,3 +1,6 @@
+export const defaultBackgroundColor = "transparent"
+export const defaultLineColor = "grey";
+export const defaultLineWidth = 0.1;
+export const numOfPoints = 50;
 export const selector: string = ".WallDrawingContainer";
-
-
+export const tooCloseValue = 15;
